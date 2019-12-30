64138eac77cc5532f0fb264aae5e63990ff5e18c
Wang Xiaodong <50005953+TummyWang@users.noreply.github.com>
2019-12-30 16:12:34 +0800

    新增导出结果功能

--- yolov3_battery/test.py
+++ yolov3_battery/test.py
@@ -1,5 +1,5 @@
 from __future__ import division
-
+from draw import detect
 from models import *
 from utils.utils import *
 from utils.datasets import *
@@ -26,16 +26,22 @@ def evaluate(model, path, iou_thres, conf_thres, nms_thres, img_size, batch_size
 #evaluate(model,path=valid_path, iou_thres=opt.iou_thres,conf_thres=opt.conf_thres,nms_thres=opt.nms_thres,img_size=opt.img_size,batch_size=8,)
     # Get dataloader
     dataset = ListDataset(path, img_size=img_size, augment=False, multiscale=False)
+    # print(dataset.image_name)
+    # print(dataset.img_files)
+    # for i, data in enumerate(dataset):
+    #     print(i, data)
+
     dataloader = torch.utils.data.DataLoader(
         dataset, batch_size=batch_size, shuffle=False, num_workers=1, collate_fn=dataset.collate_fn
     )
 
+    print("ok")
     Tensor = torch.cuda.FloatTensor if torch.cuda.is_available() else torch.FloatTensor
 
     labels = []
     sample_metrics = []  # List of tuples (TP, confs, pred)
     for batch_i, (_, imgs, targets) in enumerate(tqdm.tqdm(dataloader, desc="Detecting objects")):
-
+        # print(targets)
         # Extract labels
         labels += targets[:, 1].tolist()
         # Rescale target
@@ -52,29 +58,46 @@ def evaluate(model, path, iou_thres, conf_thres, nms_thres, img_size, batch_size
 
     # Concatenate sample statistics
     true_positives, pred_scores, pred_labels = [np.concatenate(x, 0) for x in list(zip(*sample_metrics))]
+    # print(targets)
+    # print(len(outputs))
+    # for i in range(len(true_positives)):
+    #     print(true_positives[i], pred_scores[i], pred_labels[i])
     precision, recall, AP, f1, ap_class = ap_per_class(true_positives, pred_scores, pred_labels, labels)
-
     return precision, recall, AP, f1, ap_class
-def make_test_path(dir,txt_path):
+# def make_test_path(dir,txt_path):
+#     """
+#
+#     :param dir: 测试文件根目录
+#     :param txt_path: 测试文件名称
+#     :return: 带位置的测试文件目录
+#     """
+#     with open(txt_path, "r", encoding='utf-8') as f: #打开测试文件
+#         if not os.path.exists('data/custom'):
+#             os.mkdir("data/custom")
+#         save_file="data/custom/valid.txt"
+#         file = open(save_file,'w+')
+#         py = f.readlines() #按行进行文件读取
+#
+#         for oo in py:
+#             file_dir=dir+os.sep+oo.strip()+".jpg"
+#             file.writelines(file_dir)
+#             file.write("\n")
+#         file.close()
+#         f.close()
+def make_test_path(dir):
     """
 
     :param dir: 测试文件根目录
-    :param txt_path: 测试文件名称
     :return: 带位置的测试文件目录
     """
-    with open(txt_path, "r", encoding='utf-8') as f: #逐一打开文件
-        if not os.path.exists('data/custom'):
-            os.mkdir("data/custom")
-        save_file="data/custom/valid.txt"
-        file = open(save_file,'w+')
-        py = f.readlines() #按行进行文件读取
-
-        for oo in py:
-            file_dir=dir+os.sep+oo.strip()+".jpg"
-            file.writelines(file_dir)
-            file.write("\n")
-        file.close()
-        f.close()
+    p=dir+os.sep+"*"
+    image_test=glob.glob(p)
+    save_file="data/custom/valid.txt"
+    file = open(save_file,'w+')
+    for oo in image_test:
+        file.writelines(oo)
+        file.write("\n")
+    file.close()
 
 
 
@@ -83,26 +106,28 @@ if __name__ == "__main__":
     parser.add_argument("--batch_size", type=int, default=2, help="size of each image batch")
     parser.add_argument("--model_def", type=str, default="config/yolov3-custom.cfg", help="path to model definition file")
     parser.add_argument("--data_config", type=str, default="config/custom.data", help="path to data config file")
-     parser.add_argument("--weights_path", type=str, default="weights/yolov3_ckpt_30.pth", help="path to weights file")
-#    parser.add_argument("--weights_path", type=str, default="weights/yolov3.weights", help="path to weights file")
+    parser.add_argument("--weights_path", type=str, default="yolov3_ckpt_1.pth", help="path to weights file")
+    # parser.add_argument("--weights_path", type=str, default="battery_yolov3.weights", help="path to weights file")
+    #    parser.add_argument("--weights_path", type=str, default="weights/yolov3.weights", help="path to weights file")
     parser.add_argument("--class_path", type=str, default="data/custom/classes.names", help="path to class label file")
     parser.add_argument("--iou_thres", type=float, default=0.5, help="iou threshold required to qualify as detected")
-    parser.add_argument("--conf_thres", type=float, default=0.005, help="object confidence threshold")
+    parser.add_argument("--conf_thres", type=float, default=0.01, help="object confidence threshold")
     parser.add_argument("--nms_thres", type=float, default=0.5, help="iou thresshold for non-maximum suppression")
     parser.add_argument("--n_cpu", type=int, default=8, help="number of cpu threads to use during batch generation")
     parser.add_argument("--img_size", type=int, default=416, help="size of each image dimension")
+    parser.add_argument("--image_path", type=str, default='/Users/wangxiaodong/Desktop/test/Image_test', help="image path")
+    parser.add_argument("--label_path", type=str, default='/Users/wangxiaodong/Desktop/test/Anno_test', help="Annotation path")
     opt = parser.parse_args()
-    print(opt)
+    # print(opt)
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     data_config = parse_data_config(opt.data_config)
-    image_path = data_config["image_path"]
-    test_path=data_config["test"]
-    label_path=data_config["label_path"]
-    class_names = load_classes(data_config["names"])
-    make_test_path(image_path,test_path)
-    valid_path="data/custom/valid.txt"
+    image_path = opt.image_path #获得图片位置
+    label_path=opt.label_path #获得标签位置
+    class_names = load_classes(data_config["names"]) #载入分类名称
+    valid_path=data_config["valid"]
+    make_test_path(image_path) #制作测试路径
 
     datahelper(image_path,label_path)
     print("ok")
@@ -117,7 +142,9 @@ if __name__ == "__main__":
             model.load_state_dict(torch.load(opt.weights_path)) #载入普通参数
         else:
             model.load_state_dict(torch.load(opt.weights_path,map_location=lambda storage, loc: storage))
-       
+            #  model.load_state_dict(torch.load(opt.weights_path))
+
+
 
     print("Compute mAP...") #计算mAP
 
@@ -136,3 +163,42 @@ if __name__ == "__main__":
         print(f"+ Class '{c}' ({class_names[c]}) - AP: {AP[i]}")
 
     print(f"mAP: {AP.mean()}") #计算平均AP
+    # compute coordinates of boxes
+    imgs, img_detections = detect(
+                                  model=model,
+                                  path=image_path,
+                                  conf_thres=opt.conf_thres,
+                                  nms_thres=opt.nms_thres,
+                                  img_size=opt.img_size,
+                                  batch_size=8,
+                                  n_cpu=8,
+                                  device=device,
+                                  )
+        
+    os.makedirs('predicted_file', exist_ok=True)
+        
+    class1 = open('predicted_file/det_test_core.txt', 'w')
+    class2 = open('predicted_file/det_test_coreless.txt', 'w')
+    for path, boxes in zip(imgs, img_detections):
+        w, h = Image.open(path).size
+        boxes = rescale_boxes(boxes, opt.img_size, (h, w))
+        for box in boxes:
+            line = [
+                    # os.path.split(path)[1].split('_')[1].split('.')[0],  # no prefix
+                    os.path.split(path)[1].split('.')[0].replace('coreless_','').replace('core_',''),  # with prefix 'core_' or 'coreless_'
+                    f'{box[4].tolist():.3f}',  # conf
+                    f'{box[0].tolist():.1f}',
+                    f'{box[1].tolist():.1f}',
+                    f'{box[2].tolist():.1f}',
+                    f'{box[3].tolist():.1f}',
+                    ]
+            if box[-1] == 0.0:
+                class1.write(' '.join(line) + '\n')
+            elif box[-1] == 1.0:
+                class2.write(' '.join(line) + '\n')
+    class1.close()
+    class2.close()
+    print('Output file saved.\n')
+    # os.remove('data/custom/labels')
+    os.remove('data/cache/class_url.pkl')
+    os.remove('data/custom/valid.txt')
